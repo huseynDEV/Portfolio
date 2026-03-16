5f6c4f5386da0d3c90a1cdb0430f0bde200b29f4
huseyn <huseyn@huseyn-MacBook-Air.local>
2026-03-16 07:16:35 -0700

    first commit

---
+++ 2-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,37 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+class program
+{
+
+    static void Main(string[] arr)
+    {
+        int[] eded = { 1, 2, 6, 14, 25 };
+        int c = 5;
+        int d = 15;
+        CustomArrResize(ref eded, c, d);
+        for (int i = 0; i < eded.Length; i++)
+        {
+            Console.WriteLine(eded[i]);
+        }
+
+    }
+
+    static void CustomArrResize(ref int[] arr, params int[] num)
+    {
+        int[] newArr = new int[arr.Length + num.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            newArr[i] = arr[i];
+        }
+        for (int i = 0; i < num.Length; i++)
+        {
+            newArr[arr.Length + i] = num[i];
+
+        }
+        arr = newArr;
+
+
+    }
+}
